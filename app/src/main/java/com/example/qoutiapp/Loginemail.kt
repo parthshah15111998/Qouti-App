5fa137c9b81d11ba95ba7c7d88fffb86aab1b3b2
parthshah15111998 <parths.letsnurture@gmail.com>
2022-03-17 16:35:36 +0530

    Done 3 screen

--- app/src/main/java/com/example/qoutiapp/Loginemail.kt
+++ app/src/main/java/com/example/qoutiapp/Loginemail.kt
@@ -1,16 +1,42 @@
 package com.example.qoutiapp
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
+import com.example.qoutiapp.databinding.ActivityLoginemailBinding
 
 class Loginemail : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginemailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loginemail)
+        binding = ActivityLoginemailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val intent = Intent(this, Signup::class.java)
-        startActivity(intent)
-        finish()
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        val spannable = SpannableString("New user? Register")
+        spannable.setSpan(
+            UnderlineSpan(),
+            10, // start
+            17, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD_ITALIC),
+            10, // start
+            17, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        binding.tvRegister.text = spannable
     }
 }
--- app/src/main/java/com/example/qoutiapp/Signup.kt
+++ app/src/main/java/com/example/qoutiapp/Signup.kt
@@ -1,11 +1,59 @@
 package com.example.qoutiapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
+import com.example.qoutiapp.databinding.ActivitySignupBinding
 
 class Signup : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding= ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spannable = SpannableString("By clicking on sign up button, you agree to our Terms Conditions & Privacy Policy")
+        val spannable2= SpannableString("Existing user? Login")
+        spannable.setSpan(UnderlineSpan(),
+            48, // start
+            64, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD_ITALIC),
+            48, // start
+            64, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(UnderlineSpan(),
+            67, // start
+            80, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD_ITALIC),
+            67, // start
+            80, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable2.setSpan(UnderlineSpan(),
+            15, // start
+            20, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable2.setSpan(
+            StyleSpan(Typeface.BOLD_ITALIC),
+            15, // start
+            20, // end
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        binding.tvTermCondition.text = spannable
+        binding.tvLogin.text=spannable2
     }
 }
