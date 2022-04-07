6f50a55fedc57bd1a6c7c144c80e18589495a221
parthshah15111998 <parths.letsnurture@gmail.com>
2022-04-07 19:46:11 +0530

    Done Coroutine

--- app/src/main/java/com/example/qoutiapp/api/ApiInterFace.kt
+++ app/src/main/java/com/example/qoutiapp/api/ApiInterFace.kt
@@ -3,11 +3,12 @@ package com.example.qoutiapp.api
 import com.example.qoutiapp.modelclass.LoginRequest
 import com.example.qoutiapp.modelclass.LoginResponse
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiInterFace {
 
     @POST("login")
-    fun createPost(@Body modelClass: LoginRequest): Call<LoginResponse?>?
+    suspend fun createPost(@Body modelClass: LoginRequest): Response<LoginResponse?>?
 }
--- app/src/main/java/com/example/qoutiapp/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/qoutiapp/fragment/LoginFragment.kt
@@ -18,6 +18,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import com.example.qoutiapp.BrowserActivity
 import com.example.qoutiapp.LoginSignUpActivity
@@ -27,9 +28,13 @@ import com.example.qoutiapp.api.ApiRetrofitHelper
 import com.example.qoutiapp.databinding.FragmentLoginBinding
 import com.example.qoutiapp.modelclass.LoginRequest
 import com.example.qoutiapp.modelclass.LoginResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.coroutineContext
 
 
 class LoginFragment : Fragment() {
@@ -68,22 +73,23 @@ class LoginFragment : Fragment() {
             ) {
                 Toast.makeText(context, "Please enter both the values", Toast.LENGTH_SHORT).show()
             } else {
-                binding.progressBar.visibility=View.VISIBLE
-                LoginApi(
-                    binding.etPhoneNumber.prefixText.toString(),
-                    binding.etPhoneNumber.editText?.text.toString(),
-                    binding.etPassword.editText?.text.toString()
-                )
+                binding.progressBar.visibility = View.VISIBLE
+                CoroutineScope(Dispatchers.IO).launch {
+                    LoginApi("+91","9638134029","Letsdoit@123")
+
+                }
             }//9638134029","password": "Letsdoit@123
         }
         return binding.root
     }
 
-    private fun LoginApi(country_code: String, user_mobile: String, password: String) {
+
+    private suspend fun LoginApi(country_code: String, user_mobile: String, password: String) {
 
         val apiInterFace = ApiRetrofitHelper.retrofit.create(ApiInterFace::class.java)
 
-        val call: Call<LoginResponse?>? = apiInterFace.createPost(
+
+        val response = apiInterFace.createPost(
             LoginRequest(
                 LoginRequest.Data(
                     country_code, user_mobile, password
@@ -91,44 +97,24 @@ class LoginFragment : Fragment() {
             )
         )
 
-        call!!.enqueue(object : Callback<LoginResponse?> {
-            @SuppressLint("CommitPrefEdits")
-            override fun onResponse(
-                call: Call<LoginResponse?>,
-                response: Response<LoginResponse?>
-            ) {
-                if (response.isSuccessful && response.body() != null) {
-                    val responseFromApi = response.body()!!
-                    binding.progressBar.visibility=View.GONE
-                    if (responseFromApi.status == 1) {
-                        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-                        editor.putString("countryCode", responseFromApi.data.country_code)
-                        editor.putString("mobileNumber", responseFromApi.data.user_mobile)
-                        editor.apply()
-                        val intent = Intent(activity, BrowserActivity::class.java)
-                        startActivity(intent)
-                        activity?.finish()
-                        Toast.makeText(context, responseFromApi.message, Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Toast.makeText(context, "Unsuccessful message", Toast.LENGTH_SHORT).show()
+        lifecycleScope.launch {
+            if (response!!.isSuccessful && response.body() != null) {
+                val responseFromApi = response.body()!!
+                binding.progressBar.visibility = View.GONE
+                if (responseFromApi.status == 1) {
+                    val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                    editor.putString("countryCode", responseFromApi.data.country_code)
+                    editor.putString("mobileNumber", responseFromApi.data.user_mobile)
+                    editor.apply()
+                    val intent = Intent(activity, BrowserActivity::class.java)
+                    startActivity(intent)
+                    activity?.finish()
+                    Toast.makeText(context, responseFromApi.message, Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(context, "Unsuccessful message", Toast.LENGTH_SHORT).show()
             }
-            override fun onFailure(call: Call<LoginResponse?>, t: Throwable) {
-                Toast.makeText(context, "Wrong Password And MobileNumber", Toast.LENGTH_SHORT).show()
-            }
-        })
+        }
 
     }
-
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                getActivity()?.finish()
-            }
-        })
-    }
-
 }
